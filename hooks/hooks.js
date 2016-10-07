dad461eefcb671e7a6e6e676f3b1bfa3cd6723b8
gregorybesson <gregory.besson@adfab.fr>
2016-10-07 23:27:20 +0200

    prep 2.6.0

--- hooks/hooks.js
+++ hooks/hooks.js
@@ -6,13 +6,13 @@ var path = require('path');
 var hooks = {
   afterEditorFormBlocks: function (blocks, json, abe) {
     var rex = /\{\{\{i18nAbe(.*?)lang \'(.*?)\'\}\}\}/g
-    var tpl = abe.cmsTemplate.template.getTemplate(json.abe_meta.template);
+    var tpl = abe.cmsTemplates.template.getTemplate(json.abe_meta.template);
     var matches = tpl.match(rex);
     if(matches){
       var index = 0;
       var isAlreadyAdded = [];
       blocks['i18n'] = {};
-      var jsonData = abe.FileParser.getJson(path.join(abe.config.root, 'locales', json.lang + '.json'));
+      var jsonData = abe.cmsData.file.get(path.join(abe.config.root, 'locales', json.lang + '.json'));
       matches.forEach(function (match) {
         var value = match.replace(rex, '$2').replace(/\\'/g, "'");
         if(isAlreadyAdded.indexOf(value) < 1) {
